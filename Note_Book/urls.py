ac436cff2f6c56f33fb016f65c838fa598bfc9c7
Hossein <hgholami1381@gmail.com>
2025-07-10 13:48:39 +0330

    add the new url for the main_page.

--- Note_Book/urls.py
+++ Note_Book/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('note_book/',include("Note_Book_app.urls"))
+    path('note_book/',include("Note_Book_app.urls")),
 ]
