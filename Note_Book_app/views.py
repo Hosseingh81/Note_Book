014d51fe10573475afb87db25c0269753e1bcd08
Hossein <hgholami1381@gmail.com>
2025-07-10 13:56:38 +0330

    add a new template_view cbv called main_page_view to proccess the first and the main_page.

--- Note_Book_app/views.py
+++ Note_Book_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-
+#this class is for the first page that you face.the url is ./note_book
+class main_page_view(TemplateView):
+    template_name = "main_page.html"
 
