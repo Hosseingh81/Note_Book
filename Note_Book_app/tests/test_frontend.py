d26b39b9cbbd4570060074eeb69dcdb7dec84059
Hossein <hgholami1381@gmail.com>
2025-07-13 13:59:26 +0330

    add a new test to see if links in the main_page works correctly.(its not completed yet.)

--- Note_Book_app/tests/test_frontend.py
+++ Note_Book_app/tests/test_frontend.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from .django_modelfield_tests_funcs import *
 from django.core.files.uploadedfile import SimpleUploadedFile
+import re
+
 
 
 
@@ -13,15 +15,26 @@ class Front_tests(TestCase):
     def setUp(self):
         self.url=reverse("Note_Book_app:main_page")
         self.response=self.client.get(self.url)
-        self.context='new_note\n' \
-        'pervious notes' 
         
     def test_main_page_returns_200_status_code(self): #this func tests that the mainpage returns 200 status code.
         self.assertEqual(self.response.status_code,200)
 
     def test_main_page_uses_the_correct_template(self): #this func test that the main page uses the correct template.
         self.assertTemplateUsed(self.response,template_name="main_page.html")
-        print("this context of the main page",self.response.content)
+        print("this context of the main page\n",self.response.content)
     
+    def test_main_page_link_returns_200_status_code(self): #this func test that the user will redirect to a working page with 200 status code.
+        html_bytes = self.response.content
+        html_str = html_bytes.decode('utf-8')
+        matches = re.findall(r'href="([^"]+)"', html_str)
+        for href in matches:
+            new_note_page_url=href
+
+        self.new_note_page_response=self.client.get(new_note_page_url)
+
+        self.assertEqual(self.new_note_page_response.status_code,200)
+
+                
+
     # def test_main_page_shows_correct_context(self): #this func test that the main page shows the correct context.
     #     self.assertEqual(self.response.context,self.context)
