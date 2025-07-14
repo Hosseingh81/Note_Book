f9c1f9d502516d8f0dafc15a998dc5bbb972d034
Hossein <hgholami1381@gmail.com>
2025-07-14 12:44:44 +0330

    add new paths to url file.(/new_note path and /perevious_notes path.)

--- Note_Book_app/urls.py
+++ Note_Book_app/urls.py
@@ -6,4 +6,6 @@ app_name="Note_Book_app"
 
 urlpatterns = [
     path("",views.main_page_view.as_view(),name='main_page'),
+    path("new_note/",views.new_note_view.as_view(),name='new_note'),
+    path("perevious_notes",views.perevious_notes_view.as_view(),name='perevious_notes'),
 ]
