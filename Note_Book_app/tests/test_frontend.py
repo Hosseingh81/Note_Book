bdcffd7ee4b110c85e1e147ed41893b855d55d66
Hossein <hgholami1381@gmail.com>
2025-07-14 10:54:45 +0330

    complete the (test_main_page_link_returns_200_status_code) func in test_frontend.py file.

--- Note_Book_app/tests/test_frontend.py
+++ Note_Book_app/tests/test_frontend.py
@@ -24,17 +24,15 @@ class Front_tests(TestCase):
         print("this context of the main page\n",self.response.content)
     
     def test_main_page_link_returns_200_status_code(self): #this func test that the user will redirect to a working page with 200 status code.
+        self.response_main_page_links=[]
         html_bytes = self.response.content
         html_str = html_bytes.decode('utf-8')
         matches = re.findall(r'href="([^"]+)"', html_str)
-        for href in matches:
-            new_note_page_url=href
-
-        self.new_note_page_response=self.client.get(new_note_page_url)
-
-        self.assertEqual(self.new_note_page_response.status_code,200)
-
-                
+        for i in range(0,len(matches)):
+            self.response_main_page_links.append(self.client.get(matches))
+        for i in range(0,len(self.response_main_page_links)):
+            self.assertEqual(self.response_main_page_links,200)
+ 
 
     # def test_main_page_shows_correct_context(self): #this func test that the main page shows the correct context.
     #     self.assertEqual(self.response.context,self.context)
