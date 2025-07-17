9d7ae6d2d97822f49386952611c4f417654386f9
Hossein <Hossein@email.com>
2025-07-17 12:03:41 +0330

    added another test for the new page post functionality(this func checks that the post request returns 200 code.)

--- Note_Book_app/tests/test_frontend.py
+++ Note_Book_app/tests/test_frontend.py
@@ -34,7 +34,8 @@ class Front_tests(TestCase):
 
     def test_new_note_page_post_data_returns_200_status_code(self): #this func tests that the new note page posts the data in its form return 200 status code.
         self.url_new_note= reverse("Note_Book_app:new_note")
-        self.client.post(path=self.url_new_note, data={'name':'name of the note'} )
+        new_note_page_post_res=self.client.post(path=self.url_new_note, data={'name':'name of the note'} )
+        self.assertEqual(new_note_page_post_res.status_code,200)
  
 
     # def test_main_page_shows_correct_context(self): #this func test that the main page shows the correct context.
