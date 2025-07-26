71fed0c5fa331a3e775e5ecab0c616cc01edff0b
Hossein <Hossein@email.com>
2025-07-26 12:24:09 +0330

    added a new feature that name field will auto generated when it's null from models primery key.

--- Note_Book_app/models.py
+++ Note_Book_app/models.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Note(models.Model):
-    default_name=f"note {id}"
-    name=models.CharField(default=default_name)
+    name=models.CharField(blank=True)
     note=models.TextField(verbose_name="write your note here.")
     Published_at=models.DateTimeField(auto_now_add=True)
 
@@ -14,3 +13,13 @@ class Note(models.Model):
     
     def get_absolute_url(self):
         return reverse("Note_Book_app:previous_notes")
+    
+
+    def save(self,*args, **kwargs):
+        creating=self.pk is None
+        super().save(*args, **kwargs)
+
+
+        if creating and not self.name:
+            self.name = f"note {self.pk}"
+            super().save(update_fields=['name'])
