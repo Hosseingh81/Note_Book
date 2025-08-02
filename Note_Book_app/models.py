bf9847c691c4db0eee6c6c7438c661f85bc46871
Hossein <Hossein@email.com>
2025-08-02 13:42:01 +0330

    add a new feild called Updated_at and migrate it.

--- Note_Book_app/models.py
+++ Note_Book_app/models.py
@@ -11,6 +11,7 @@ class Note(models.Model):
     name=models.CharField(blank=True) # this is the name field of the model.
     note=models.TextField(verbose_name="write your note here.") #this is the note field of the model.
     Published_at=models.DateTimeField(auto_now_add=True) #this is published_at field of the model that saves the time whenever note is published.
+    Update_at=models.DateTimeField(auto_now=True) #this is Upadted_at field of the model that stores the time when the object is updated.
 
     def __str__(self): # shows the name field of the note.
         return self.name
