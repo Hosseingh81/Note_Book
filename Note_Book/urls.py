2f1a7d135cfef8ff4f9d44494b2b035a5122b70a
Hossein <Hossein@email.com>
2025-08-12 13:41:32 +0330

    add user mangement to note_book_app.(just login and logout template with password change.),nothing on perimissions.

--- Note_Book/urls.py
+++ Note_Book/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('note_book/',include("Note_Book_app.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- Note_Book_app/urls.py
+++ Note_Book_app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-
+from django.urls import path,include
 from . import views
+
 
 app_name="Note_Book_app"
 
